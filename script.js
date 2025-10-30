f21e76f6f15219fcdf89318d77e60a441098f494
pierfilippodiviccaro <diviccarofilippo@gmail.com>
2025-10-30 18:33:04 +0100

    fix: sistemato un bug nell'esercizio

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 // gioco dei dadi 
 //generare un numero random da 1 a 6  per il giocatore e il computer e vedere qual'è il più altro tra i due e il più alto vince
-    //  let dadoPlayer = Math.floor((Math.random()*6)+1)
-    //  let dadoComp = Math.floor((Math.random()*6)+1)
-    //  if (dadoPlayer > dadoComp  ){
-    //     console.log("ha vinto il giocatore!")
-    //  }
-    //  else{
-    //     console.log("mi spiace ma ha vinto il computer")
-    //  }
+//  let dadoPlayer = Math.floor((Math.random()*6)+1)
+//  let dadoComp = Math.floor((Math.random()*6)+1)
+//  if (dadoPlayer > dadoComp  ){
+//     console.log("ha vinto il giocatore!")
+//  }
+//  else{
+//     console.log("mi spiace ma ha vinto il computer")
+//  }
 //primo esercizio completato 
 //secondo esercizio: Crea un array con delle email di chi può accedere.
 // Chiedi all’utente la sua email,
 // controlla che sia nella lista di chi può accedere,
 // stampa un messaggio appropriato sull’esito del controllo.
 // Non è necessario provvedere alla validazione delle email.
-const emails = ["miaomiao@gmail.com", "asdrubalino@gmail.com","paperino@hotmail.boh","genoveffo@coccode.gallina"]
- const userMail = prompt("aggiungi una mail")
- let mail = false
-for (i=0;  i > emails.legnth; i++){
- if (userMail === emails[i]){
-   emails = true;
-   break;
+const emails = ["miaomiao@gmail.com", "asdrubalino@gmail.com", "paperino@hotmail.boh", "genoveffo@coccode.gallina"]
+const userMail = prompt("aggiungi una mail")
+let isMailFound = false
+for (i = 0; i < emails.length; i++) {
+    if (userMail === emails[i]) {
+        isMailFound = true;
+    }
 }
-}
-if(mail){
+
+if (isMailFound) {
     console.log("smettila di fare miao miao");
 }
-else{
-     console.log("accesso negato");
+else {
+    console.log("accesso negato");
 }
