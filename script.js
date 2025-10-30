47dd24884aaaafee5ce0e116034afe5fe7f27a06
pierfilippodiviccaro <diviccarofilippo@gmail.com>
2025-10-30 16:38:50 +0100

    feat: fatta prima parte del secondo esercizio che è ancora da finire

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 // gioco dei dadi 
 //generare un numero random da 1 a 6  per il giocatore e il computer e vedere qual'è il più altro tra i due e il più alto vince
-     let dadoPlayer = Math.floor((Math.random()*6)+1)
-     let dadoComp = Math.floor((Math.random()*6)+1)
-     if (dadoPlayer > dadoComp  ){
-        console.log("ha vinto il giocatore!")
-     }
-     else{
-        console.log("mi spiace ma ha vinto il computer")
-     }
-
+    //  let dadoPlayer = Math.floor((Math.random()*6)+1)
+    //  let dadoComp = Math.floor((Math.random()*6)+1)
+    //  if (dadoPlayer > dadoComp  ){
+    //     console.log("ha vinto il giocatore!")
+    //  }
+    //  else{
+    //     console.log("mi spiace ma ha vinto il computer")
+    //  }
+//primo esercizio completato 
+//secondo esercizio: Crea un array con delle email di chi può accedere.
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+// Non è necessario provvedere alla validazione delle email.
+const emails = ["miaomiao@gmail.com", "asdrubalino@gmail.com","paperino@hotmail.boh","genoveffo@coccode.gallina"]
+ const userMail = prompt("aggiungi una mail")
+for (i=0;  i >= emails.legnth; i++){
+ if (userMail == emails[0]){
+    console.log("smettila di fare miaomiao")
+    }
+    else{
+        console.log("inserisci un altra mail che sia tra queste")
+    }
+}
